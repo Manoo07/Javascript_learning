20a0325e78159a1d81817d741457b3db57c0adc6
Manoo07 <manoharboinapalli1196@gmail.com>
2024-01-06 12:57:42 +0530

    Added debouncing

--- ind.js
+++ ind.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = 3003;
+
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+
+// // Your routes and other middleware go here
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
 app.get("/sum",(req,res)=>{
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
